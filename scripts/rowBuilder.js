1772c281228101b1666fcce3db40dd6fbe62e77a
Dimags500 <73039506+Dimags500@users.noreply.github.com>
2022-05-11 16:04:39 +0300

    css class added

--- scripts/rowBuilder.js
+++ scripts/rowBuilder.js
@@ -1,7 +1,7 @@
 export function rowBuilder(data, table) {
   data.forEach((item) => {
     let tr = document.createElement("tr");
-
+    tr.classList.add("row");
     for (value of Object.values(item)) {
       let td = document.createElement("td");
 
